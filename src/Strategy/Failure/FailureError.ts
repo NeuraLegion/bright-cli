eb1a5e80eab6dcd1f04ce79a6e23197d40fd517d
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2019-09-17 14:15:38 +0300

    feat(Failure): implement base failure strategies and polling

---
+++ src/Strategy/Failure/FailureError.ts
@@ -0,0 +1,6 @@
+export class FailureError extends Error {
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
---
+++ src/Strategy/Failure/FailureOnFirstIssue.ts
@@ -0,0 +1,23 @@
+import { StatsIssuesCategory } from './Polling';
+import { FailureStrategy } from './FailureStrategy';
+import { FailureError } from './FailureError';
+
+export class FailureOnFirstIssue extends FailureStrategy {
+  constructor() {
+    super();
+  }
+
+  protected exceptionOnFailure(stat: StatsIssuesCategory): never {
+    throw new FailureError(`NexPloit found a first issue.`, 50);
+  }
+
+  protected isFailureCondition(stats: StatsIssuesCategory | null): boolean {
+    return stats && stats.number > 0;
+  }
+
+  protected selectStatIssueCategory(
+    issuesStats: StatsIssuesCategory[]
+  ): StatsIssuesCategory {
+    return issuesStats.find((stats: StatsIssuesCategory) => stats.number > 0);
+  }
+}
