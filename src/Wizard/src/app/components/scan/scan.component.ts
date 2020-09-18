02c67c15ddf60715f6a8c9f6f3b3a4aab2bef37c
MariaGunko <maria.gunko@sogoservices.com>
2020-09-18 17:06:52 +0300

    scan messages updated and next button disabled in case scan not started or not succeeded

--- src/Wizard/src/app/components/scan/scan.component.ts
+++ src/Wizard/src/app/components/scan/scan.component.ts
@@ -15,12 +15,22 @@ export class ScanComponent {
 
   targetUrl: string = '{{url}}';
   progressMsg = '';
+  tryMsg = '';
+  scanStarted = false;
 
   onSubmit(): void {
+    this.tryMsg = `Trying to reach ${this.targetUrl}...`;
     this.service.startScan(this.targetUrl).subscribe((response: any) => {
+      this.scanStarted = true;
       this.service.saveScanId(response.scanId);
-      this.progressMsg = `Trying to reach ${this.targetUrl}...`;
+      this.progressMsg = `Communication test to ${this.targetUrl} completed successfully, and a demo scan has
+      started, click on Next to continue.`;
     }, error => {
+      this.progressMsg = `Connection to ${this.targetUrl} is blocked, please verify that the machine on
+      which the Repeater is installed can reach the target server.
+      Possible reasons for communication failure:
+      ● Outbound communication to the host is blocked by a Firewall or network
+      settings`;
       console.log (error);
     });
   }
