1b51d7b176836a2afb562950b2b1c75b6f6fd390
MariaGunko <maria.gunko@sogoservices.com>
2020-09-23 14:49:23 +0300

    reactive forms implemented on main page

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { ScanComponent } from './components/scan/scan.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, MainComponent, ScanComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- public/src/app/components/main/main.component.ts
+++ public/src/app/components/main/main.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Tokens } from 'src/app/app.model';
 import { AppService } from 'src/app/app.service';
 import { VisibilityToggle } from '../../shared/VisibilityToggle';
+
+export enum FormFields {
+  AUTH_TOKEN = 'authToken',
+  REPEATER_ID = 'repeaterId'
+}
 
 @Component({
   selector: 'app-main',
@@ -12,33 +18,41 @@ import { VisibilityToggle } from '../../shared/VisibilityToggle';
 export class MainComponent implements OnInit {
   visibilityToggle = new VisibilityToggle();
   title = 'Connectivity Wizard';
-  authToken: string;
-  repeaterId: string;
+  mainForm: FormGroup;
 
   constructor(private router: Router,
+              private formBuilder: FormBuilder,
               protected service: AppService) {}
 
   ngOnInit() {
     console.log('Welcome');
+    this.initForm();
     this.service.getTokens().subscribe((response: Tokens) => {
       if (response.authToken !== '' && response.repeaterId !== '') {
         this.visibilityToggle.toggleEye('token', 'toggleEye-1');
         this.visibilityToggle.toggleEye('repeater', 'toggleEye-2');
       }
-      this.authToken = response.authToken;
-      this.repeaterId = response.repeaterId;
+      this.mainForm.controls[FormFields.AUTH_TOKEN].setValue(response.authToken);
+      this.mainForm.controls[FormFields.REPEATER_ID].setValue(response.repeaterId);
     }, error => {
       console.log (error);
     });
   }
 
+  initForm(): void{
+    this.mainForm = this.formBuilder.group({
+      authToken: ['', [Validators.required]],
+      repeaterId: ['', [Validators.required]],
+    });
+  }
+
   onSubmit(): void {
-    if (this.authToken === null || this.repeaterId == null) {
+    if (this.mainForm.controls[FormFields.AUTH_TOKEN].value === null || this.mainForm.controls[FormFields.REPEATER_ID].value == null) {
       console.log ('Error, tokens are invalid');
     } else {
       const actionPayload = {
-        authToken: this.authToken,
-        repeaterId: this.repeaterId
+        authToken: this.mainForm.controls[FormFields.AUTH_TOKEN].value,
+        repeaterId: this.mainForm.controls[FormFields.REPEATER_ID].value
       };
       this.service.saveTokens(actionPayload).subscribe((response: any) => {
         this.router.navigateByUrl('diagnostics');
