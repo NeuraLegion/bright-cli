83a11c6c93386340d55ec7a937cb2258e8d43c54
cruisade <kirill.ignatov@neuralegion.com>
2021-03-14 21:17:26 +0300

    fix: network testing windows fix

--- src/Handlers/NetworkTestHandler.ts
+++ src/Handlers/NetworkTestHandler.ts
@@ -2,9 +2,9 @@ import { bind, Handler } from '../Bus';
 import { NetworkTest } from './Events';
 import { NetworkTestResult } from '../Integrations';
 import { ReadlinePlatform } from '../Wizard';
-import { Helpers } from '../Utils';
 import { injectable } from 'tsyringe';
 import { spawn } from 'nexpect';
+
 
 @injectable()
 @bind(NetworkTest)
@@ -21,17 +21,18 @@ export class NetworkTestHandler
 
   private async getOutput(urls: string[]): Promise<string> {
     return new Promise((resolve, reject) => {
-      const args = Helpers.getExecArgs({
-        excludeAll: true,
-        include: ['configure', '--nogui', '--network-only']
-      });
-
-      spawn(args.command, args.args)
+      const commandIndex = process.argv.indexOf('repeater');
+      const args = process.argv
+        .filter((_, i)=> i < commandIndex)
+        .concat(['configure', '--nogui', '--network-only']);
+      spawn(args.join(' '))
         .wait(ReadlinePlatform.URL_QUESTION)
         .sendline(urls.join(','))
         .wait(ReadlinePlatform.COMPELED_MESSAGE)
         .sendEof()
         .run((err, output, exit) => {
+          console.log(err, output, exit);
+
           if (err) {
             return reject(err);
           }
