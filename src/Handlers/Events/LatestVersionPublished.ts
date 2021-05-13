25089eb4bd16fc3a02d11b22f30fa8d074cb7289
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2021-05-13 22:08:38 +0300

    feat(repeater): disallow using the repeater with different versions

--- src/Handlers/Events/LatestVersionPublished.ts
+++ src/Handlers/Events/LatestVersionPublished.ts
@@ -2,7 +2,8 @@ import { Event } from '../../Bus';
 
 export class LatestVersionPublished implements Event {
   constructor(
+    public readonly repeaterId: string,
     public readonly version: string,
-    public readonly needToBeUpdated: boolean
+    public readonly lastUsedVersion: string
   ) {}
 }
--- src/Handlers/LatestVersionPublishedHandler.ts
+++ src/Handlers/LatestVersionPublishedHandler.ts
@@ -3,6 +3,7 @@ import { LatestVersionPublished } from './Events';
 import { logger } from '../Utils';
 import { injectable } from 'tsyringe';
 import chalk from 'chalk';
+import { eq, gt, major } from 'semver';
 
 @injectable()
 @bind(LatestVersionPublished)
@@ -10,19 +11,31 @@ export class LatestVersionPublishedHandler
   implements Handler<LatestVersionPublished> {
   public async handle({
     version,
-    needToBeUpdated
+    lastUsedVersion,
+    repeaterId
   }: LatestVersionPublished): Promise<void> {
-    logger.warn(
-      '%s: A new Repeater version (%s) is available, for update instruction visit https://kb.neuralegion.com/#/guide/np-cli/installation',
-      chalk.yellow('(!) IMPORTANT'),
-      version
-    );
+    if (gt(version, lastUsedVersion)) {
+      logger.warn(
+        '%s: A new Repeater version (%s) is available, for update instruction visit https://kb.neuralegion.com/#/guide/np-cli/installation',
+        chalk.yellow('(!) IMPORTANT'),
+        version
+      );
+    }
 
-    if (needToBeUpdated) {
+    if (major(version) > major(lastUsedVersion)) {
       logger.error(
         '%s: The current running version is no longer supported',
         chalk.red('(!) CRITICAL')
       );
+      process.exit(1);
+    }
+
+    if (!eq(lastUsedVersion, version)) {
+      logger.error(
+        'Access Refused: There is an already running Repeater with ID %s, but with a different version',
+        repeaterId
+      );
+
       process.exit(1);
     }
   }
