604eca7c505d49e6b52142d7927097c48ea862e0
Maksym Hysko <hysko.mv@gmail.com>
2021-06-28 18:24:48 +0300

    fix(repeater): invalid version on run (#233)
    closes #232

--- src/Handlers/Events/RepeaterRegistered.ts
+++ src/Handlers/Events/RepeaterRegistered.ts
@@ -1,6 +1,7 @@
 import { Event } from '../../Bus';
 
 export enum RepeaterRegisteringError {
+  NOT_FOUND = 'not_found',
   NOT_ACTIVE = 'not_active',
   BUSY = 'busy',
   REQUIRES_TO_BE_UPDATED = 'requires_to_be_updated'
