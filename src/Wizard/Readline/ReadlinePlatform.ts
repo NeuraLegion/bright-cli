b20e8f419787cafa46500bfedc9e271e4875f215
UncleVic <vic.polyakov@gmail.com>
2021-10-12 14:30:20 +0300

    fix: remove ansi esc codes

--- src/Wizard/Readline/ReadlinePlatform.ts
+++ src/Wizard/Readline/ReadlinePlatform.ts
@@ -168,8 +168,8 @@ export class ReadlinePlatform implements Platform {
 
     if (process.platform === 'win32') {
       console.log(
-        `\x1b[33mNote:\x1b[0m Some Windows users might need to allow the ICMP network traffic through a firewall to enable this functionality.
-      For more information, see: \x1b[4mhttps://docs.neuralegion.com/docs/testing-network-connectivity\x1b[0m${EOL}`
+        `Note: Some Windows users might need to allow the ICMP network traffic through a firewall to enable this functionality.
+      For more information, see: https://docs.neuralegion.com/docs/testing-network-connectivity${EOL}`
       );
     }
 
