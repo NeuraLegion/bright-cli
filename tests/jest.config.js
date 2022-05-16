d32a1a45175ae188832c82830e553d38b4b51a38
domayorov <domayorov@gmail.com>
2022-05-16 15:40:30 +0400

    test: fix script env and create e2e jest.config.js

---
+++ tests/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json'
+    }
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'json']
+};
