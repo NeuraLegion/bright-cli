b3e9ea58d0dc66a9100142fe8c687bff18a72ec0
Siarhei Melnik <57074509+m5l14i11@users.noreply.github.com>
2023-02-16 11:55:38 +0300

    fix(archive): add file size constraints (#371)
    closes #370

--- src/Archive/Parsers/BaseParser.ts
+++ src/Archive/Parsers/BaseParser.ts
@@ -1,18 +1,27 @@
 import { Validator } from '../Validators';
 import { File, Parser } from './Parser';
 import { promisify } from 'util';
-import { access as exists, constants, readFile as read } from 'fs';
+import { access as exists, constants, readFile as read, stat } from 'fs';
 import { ok } from 'assert';
 import { basename, extname } from 'path';
 
 const readFile = promisify(read);
 const access = promisify(exists);
+const statAsync = promisify(stat);
 
 export class BaseParser<T> implements Parser {
+  private readonly FILE_SIZE_LIMIT = 500 * 1024 ** 2;
+
   constructor(protected readonly validator: Validator<T>) {}
 
   public async parse(path: string): Promise<File> {
     await this.access(path);
+
+    const fileStat = await statAsync(path);
+
+    if (fileStat.size > this.FILE_SIZE_LIMIT) {
+      throw new Error('There is not enough storage space to save this file');
+    }
 
     const content: string = await readFile(path, 'utf8');
     const ext: string = extname(path);
