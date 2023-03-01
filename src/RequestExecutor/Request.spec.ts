ba15853a08d00bbd57e57fa37225baa6d3ebec98
Aleksandr Borovsky <kycok.6o6pa@gmail.com>
2023-03-01 10:32:50 +0100

    feat(request): add setCerts describe block inside of Request spec
    closes #376

--- src/RequestExecutor/Request.spec.ts
+++ src/RequestExecutor/Request.spec.ts
@@ -6,7 +6,7 @@ describe('Request', () => {
   let Request!: typeof RequestInterface;
   const noError: NodeJS.ErrnoException | null = null;
   const certContent = Buffer.from([]);
-  
+
   beforeEach(async () => {
     readFileMock = jest.fn();
     jest.doMock('fs', () => ({ readFile: readFileMock }));
@@ -19,75 +19,77 @@ describe('Request', () => {
     readFileMock.mockReset();
   });
 
-  it.each(['https://foo.bar', 'wss://foo.bar'])(
-    'should read cert for url %s if there was matching certificate configured globally',
-    async (url) => {
-      //arrange
-      const cert = {
-        path: '~/cert.pfx',
-        hostname: 'foo.bar'
-      };
-      const request = new Request({ url, headers: {} });
-      //arrange:mock
-      readFileMock.mockImplementation((filePath, callback) =>
-        filePath === cert.path
-          ? callback(noError, certContent)
-          : callback(new Error('no such file'))
-      );
-      //act
-      await request.setCerts([cert]);
-      //assert
-      expect(request.pfx).toBeTruthy();
-    }
-  );
+  describe('setCerts', () => {
+    it.each(['https://foo.bar', 'wss://foo.bar'])(
+      'should read cert for url %s if there was matching certificate configured globally',
+      async (url) => {
+        //arrange
+        const cert = {
+          path: '~/cert.pfx',
+          hostname: 'foo.bar'
+        };
+        const request = new Request({ url, headers: {} });
+        //arrange:mock
+        readFileMock.mockImplementation((filePath, callback) =>
+          filePath === cert.path
+            ? callback(noError, certContent)
+            : callback(new Error('no such file'))
+        );
+        //act
+        await request.setCerts([cert]);
+        //assert
+        expect(request.pfx).toBeTruthy();
+      }
+    );
 
-  it.each(['https://foo.bar', 'wss://foo.bar'])(
-    'should not read cert for url %s if there was no matching by hostname',
-    async (url) => {
-      //arrange
-      const cert = {
-        path: '~/cert.pfx',
-        hostname: 'not-a-foo.bar'
-      };
-      const request = new Request({ url, headers: {} });
-      //arrange:mock
-      readFileMock.mockImplementation((filePath, callback) =>
-        filePath === cert.path
-          ? callback(noError, certContent)
-          : callback(new Error('no such file'))
-      );
-      //act
-      await request.setCerts([cert]);
-      //assert
-      expect(request.pfx).toBeUndefined();
-    }
-  );
+    it.each(['https://foo.bar', 'wss://foo.bar'])(
+      'should not read cert for url %s if there was no matching by hostname',
+      async (url) => {
+        //arrange
+        const cert = {
+          path: '~/cert.pfx',
+          hostname: 'not-a-foo.bar'
+        };
+        const request = new Request({ url, headers: {} });
+        //arrange:mock
+        readFileMock.mockImplementation((filePath, callback) =>
+          filePath === cert.path
+            ? callback(noError, certContent)
+            : callback(new Error('no such file'))
+        );
+        //act
+        await request.setCerts([cert]);
+        //assert
+        expect(request.pfx).toBeUndefined();
+      }
+    );
 
-  it.each([
-    { url: 'https://foo.bar', wildcard: '*.bar' },
-    { url: 'wss://foo.bar', wildcard: '*.bar' }
-  ])(
-    'should read cert for url $url if there was wildcard hostname match',
-    async ({ url, wildcard }) => {
-      //arrange
-      const cert = {
-        path: '~/cert.pfx',
-        hostname: wildcard
-      };
-      const request = new Request({
-        url,
-        headers: {}
-      });
-      //arrange:mock
-      readFileMock.mockImplementation((filePath, callback) =>
-        filePath === cert.path
-          ? callback(noError, certContent)
-          : callback(new Error('no such file'))
-      );
-      //act
-      await request.setCerts([cert]);
-      //assert
-      expect(request.pfx).toBeTruthy();
-    }
-  );
+    it.each([
+      { url: 'https://foo.bar', wildcard: '*.bar' },
+      { url: 'wss://foo.bar', wildcard: '*.bar' }
+    ])(
+      'should read cert for url $url if there was wildcard hostname match',
+      async ({ url, wildcard }) => {
+        //arrange
+        const cert = {
+          path: '~/cert.pfx',
+          hostname: wildcard
+        };
+        const request = new Request({
+          url,
+          headers: {}
+        });
+        //arrange:mock
+        readFileMock.mockImplementation((filePath, callback) =>
+          filePath === cert.path
+            ? callback(noError, certContent)
+            : callback(new Error('no such file'))
+        );
+        //act
+        await request.setCerts([cert]);
+        //assert
+        expect(request.pfx).toBeTruthy();
+      }
+    );
+  });
 });
