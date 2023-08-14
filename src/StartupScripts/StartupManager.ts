65121b8a86f4a64b03395755d5d3a7b7ee478981
Artem Derevnjuk <artem.derevnjuk@brightsec.com>
2023-08-14 15:35:22 +0400

    feat: support for running repeater/integration as a service on macOS (#432)
    closes #431

--- src/StartupScripts/StartupManager.ts
+++ src/StartupScripts/StartupManager.ts
@@ -2,6 +2,8 @@ import { StartupOptions } from './StartupOptions';
 
 export interface StartupManager {
   install(opts: StartupOptions): Promise<void>;
-  run(): Promise<void>;
+  run(dispose?: () => Promise<unknown> | unknown): Promise<void>;
   uninstall(name: string): Promise<void>;
 }
+
+export const StartupManager: unique symbol = Symbol('StartupManager');
