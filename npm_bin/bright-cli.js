9b142861c851bef91e40d631da22e0e3b4073093
Aleksandr Borovskii <aleksandr.borovskii@brightsec.com>
2026-02-01 23:27:58 +0100

    fix: simplify npm wrapper

--- npm_bin/bright-cli.js
+++ npm_bin/bright-cli.js
@@ -1,34 +1,27 @@
 #!/usr/bin/env node
 'use strict';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { spawn } = require('child_process');
 
 const ENTRY = path.join(__dirname, '../dist/index.js');
-const FLAG = '--max-http-header-size=32768';
+const FLAG = '--max-http-header-size=32769';
 
-// Prevent infinite recursion when we re-exec
-if (process.env.BRIGHT_CLI_NPM_WRAPPER !== '1') {
-  const args = [FLAG, ENTRY, ...process.argv.slice(2)];
+const args = [FLAG, ENTRY, ...process.argv.slice(2)];
 
-  const child = spawn(process.execPath, args, {
-    stdio: 'inherit',
-    env: { ...process.env, BRIGHT_CLI_NPM_WRAPPER: '1' }
-  });
+const child = spawn(process.execPath, args, { stdio: 'inherit' });
 
-  child.on('exit', (code, signal) => {
-    if (signal) process.kill(process.pid, signal);
-    process.exit(code ?? 0);
-  });
+child.on('exit', (code, signal) => {
+  if (signal) process.kill(process.pid, signal);
+  process.exit(code ?? 0);
+});
 
-  child.on('error', (err) => {
-    // eslint-disable-next-line no-console
-    console.error(err);
-    process.exit(1);
-  });
+child.on('error', (err) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+  process.exit(1);
+});
 
-  return;
-}
-
-// If we’re already re-exec’d, just run the real CLI entry
-require(ENTRY);
+return;
